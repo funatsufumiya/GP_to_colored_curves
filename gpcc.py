e9e3e0868bd7ae62b1863b3a536639e96ee46515
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-06-24 16:03:11 +0900

    chore: code cleanup

--- gpcc.py
+++ gpcc.py
@@ -28,12 +28,10 @@ def version():
     dt = datetime.datetime.fromtimestamp(_stat.st_mtime)
     return dt.strftime('%Y/%m/%d %H:%M:%S.%f')
 
-# https://blender.stackexchange.com/questions/51290/how-to-add-empty-object-not-using-bpy-ops
 def make_empty(name: str, location: Vector, context: Context):
     empty_obj = bpy.data.objects.new( "empty", None, )
     empty_obj.name = name
     empty_obj.empty_display_size = 1 
-    # bpy.data.collections[coll_name].objects.link(empty_obj)
     context.collection.objects.link(empty_obj)
     empty_obj.location = location
     return empty_obj
@@ -96,30 +94,30 @@ def convertCurveToMesh(
     deleteObject(curveObj)
     return new_obj
 
-def color_to_vertices(
-        meshObj: Object,
-    ):
-    assert meshObj.type == "MESH"
-    mesh = meshObj.data
+# def color_to_vertices(
+#         meshObj: Object,
+#     ):
+#     assert meshObj.type == "MESH"
+#     mesh = meshObj.data
 
-    if not mesh.attributes.active_color:
-       color_layer = mesh.color_attributes.new("Col", 'FLOAT_COLOR', 'POINT')
-    else:
-       color_layer = mesh.attributes.active_color
+#     if not mesh.attributes.active_color:
+#        color_layer = mesh.color_attributes.new("Col", 'FLOAT_COLOR', 'POINT')
+#     else:
+#        color_layer = mesh.attributes.active_color
 
-    zs = np.array([v.co[1] for v in mesh.vertices])
-    z_max = np.max(zs)
-    z_min = np.min(zs)
-    for polygon in mesh.polygons:
-        for i, vi in zip(polygon.loop_indices, polygon.vertices):
-            if i < len(color_layer.data):
-                z = zs[vi]
-                # color_layer.data[i].color = random_color()
-                # color_layer.data[i].color = get_color(i, len(color_layer.data))
-                color_layer.data[i].color = get_color(z, z_min, z_max)
+#     zs = np.array([v.co[1] for v in mesh.vertices])
+#     z_max = np.max(zs)
+#     z_min = np.min(zs)
+#     for polygon in mesh.polygons:
+#         for i, vi in zip(polygon.loop_indices, polygon.vertices):
+#             if i < len(color_layer.data):
+#                 z = zs[vi]
+#                 # color_layer.data[i].color = random_color()
+#                 # color_layer.data[i].color = get_color(i, len(color_layer.data))
+#                 color_layer.data[i].color = get_color(z, z_min, z_max)
 
-    # for i in range(len(color_layer.data)):
-    #     color_layer.data[i].color = random_color()
+#     # for i in range(len(color_layer.data)):
+#     #     color_layer.data[i].color = random_color()
 
 def rgb_to_rgba(rgb: bpy_prop_array, alpha: float) -> list[float]:
     """
@@ -163,83 +161,40 @@ def get_color(v: float, min: float, max: float):
 def gp2curves():
     break_ret = {'FINISHED'}
     last_ret = {'FINISHED'}
-    # bpy.ops.mesh.primitive_ico_sphere_add()
-    # log("created ico sphere")
 
     frame_current = bpy.context.scene.frame_current
 
     sels = bpy.context.selected_objects
-    # log(sel)
 
     if len(sels) < 1:
         return break_ret
     
     sel = sels[0]
-    # obj_name = sel.name
-    # obj_location = sel.location
-    # obj_rot_euler = sel.rotation_euler
-    # obj_scale = sel.scale
     obj_matrix_world = sel.matrix_world
     obj_type = sel.type
 
-    # log(f"rot_quat: {obj_rot_quat}")
-    
-    ## log(type(sel))
-    # log(obj_type)
-
     if obj_type != "GPENCIL":
         return break_ret
-    
-    # log("yeah! gpencil!")
-
-    # log(dir(sel.data))
 
     name = sel.data.name
     layers = sel.data.layers
 
-    # log(f"name: {name}")
-    # log(f"layers: {layers}")
-
     if len(layers) > 0:
         layer = layers[0]
-        # log(f"layer0: {layer}")
-        ## log(f"layer0 dir: {dir(layer0)}")
-        # log(f"frames: {layer.frames}")
         if len(layer.frames) > 0:
-            # get frame at current frame, by where condition
             frames_match = list(filter(lambda f: f.frame_number == frame_current, layer.frames))
             if len(frames_match) > 0:
                 frame = frames_match[0]
-                # log(f"frame: {frame}")
-                ## log(f"frame dir: {dir(frame)}")
-                # log(f"strokes: {frame.strokes}")
-                ## log(f"strokes dir: {dir(frame.strokes)}")
-                ## log(f"strokes values: {frame.strokes.values()}")
 
                 strokes = frame.strokes.values()
 
                 for stroke in strokes:
-                    # log(f"stroke: {stroke}")
                     cs = [p.co for p in stroke.points.values()]
                     vcs = [p.vertex_color for p in stroke.points.values()]
                     alphas = [p.strength for p in stroke.points.values()]
                     pressures = [p.pressure for p in stroke.points.values()]
                     thickness_factor = 0.05
                     thicknesses = np.array(pressures) * float(stroke.line_width) * thickness_factor
-                    # log(f"points: {cs}")
-
-                    # # make empty at center point
-                    # # FIXME: this point is relative to object transform
-                    # center_point = Vector([
-                    #     np.mean([c.x for c in cs]),
-                    #     np.mean([c.y for c in cs]),
-                    #     np.mean([c.z for c in cs]),
-                    # ])
-                    # log(f"p0: {cs[0]}")
-                    # log(f"p0 (x, y, z): {cs[0].x}, {cs[0].y}, {cs[0].z}")
-                    # log(f"center_point: {center_point}")
-
-                    # make_empty("test_empty", center_point, bpy.context)
 
                     curveObj = make_curves(
                         name=f"Curve_{name}",
@@ -249,12 +204,9 @@ def gp2curves():
                         radiuses=thicknesses,
                         context=bpy.context)
 
-                    # selectObject(curveObj)
-
                     meshObj = convertCurveToMesh(curveObj=curveObj, context=bpy.context)
                     selectObject(meshObj)
 
-                    # color_to_vertices(meshObj)
                     color_to_vertices_from_gp(meshObj, vcs, alphas)
 
     return last_ret
